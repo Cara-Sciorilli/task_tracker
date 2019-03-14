6e6c2df563cf9104bdabe15c61e81a07c5c2cbae
Cara-Sciorilli <csciorilli@gmail.com>
2019-03-13 23:57:27 -0400

    Added Managers field adn fixed Register User btn

--- priv/repo/migrations/20190228200250_create_users.exs
+++ priv/repo/migrations/20190228200250_create_users.exs
@@ -4,6 +4,7 @@ defmodule TaskTracker.Repo.Migrations.CreateUsers do
   def change do
     create table(:users) do
       add :name, :string, null: false
+      add :manager_id, references(:users, on_delete: :delete_all), null: false
 
       timestamps()
     end
