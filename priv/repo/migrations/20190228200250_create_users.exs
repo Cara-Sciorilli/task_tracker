6f449adaa67daf031147424c0d34480c0a821048
Cara-Sciorilli <csciorilli@gmail.com>
2019-03-14 22:38:34 -0400

    Add funtionality

--- priv/repo/migrations/20190228200250_create_users.exs
+++ priv/repo/migrations/20190228200250_create_users.exs
@@ -4,7 +4,7 @@ defmodule TaskTracker.Repo.Migrations.CreateUsers do
   def change do
     create table(:users) do
       add :name, :string, null: false
-      add :manager_id, references(:users, on_delete: :delete_all), null: false
+      add :manager_id, references(:users, on_delete: :delete_all)
 
       timestamps()
     end
